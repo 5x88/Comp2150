ad8414ae8aa468562d290cfc33b5e0569382cd40
mike alcock <mikejra@hotmail.com>
2016-01-17 15:44:25 -0600

    Change

--- Comp2150/src/A1Main.java
+++ Comp2150/src/A1Main.java
@@ -1,9 +1,15 @@
+import javax.swing.JOptionPane;
 
 public class A1Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		
+		
+		JOptionPane.showMessageDialog(null, "Hi There");
+		
+		
 	}
 
 }
